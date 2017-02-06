c59a58a1fd0d111b9d93faa6aebc1b04ab3ffc62
jsjqian <jack.qian97@gmail.com>
2017-02-06 13:17:28 -0500

    Add smooth scrolling

--- js/index.js
+++ js/index.js
@@ -12,3 +12,20 @@ function adjustLogo(){
   half = half - (document.getElementById("logo").height/2);
   document.getElementById("logo").style.marginTop = half.toString() + "px";
 }
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
